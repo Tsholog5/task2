7140b2f399752aab939a9b1ae82bd1713714afe8
bootcamp participant <bootcampParticipant@example.com>
2024-05-03 14:28:34 +0200

    css intergration

--- index.js
+++
@@ -1,8 +0,0 @@
-<header class="header">
-        <a href="index.html" class="logo"><span>Tsholo</span>Gaarekwe</
-         <nav class="navbar">
-            <a href="index.html" class="active">Home</a>
-            <a href="about.html">About Me</a>
-            <a href="contact.html">Contact Me</a>
-            </nav>
-    </header>
